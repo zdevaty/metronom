5e79017ddf431968045ffa2a234c599c73e99466
Zdenek Devaty <zdenek.devaty@gmail.com>
2026-04-06 20:33:19 +0200

    Zvukovka

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./frontend"))
-	http.Handle("/", fs)
+	// Serve live version at root
+	liveFS := http.FileServer(http.Dir("./frontend"))
+	http.Handle("/", liveFS)
+
+	// Serve sound check version at /zvukovka
+	zvukovkaFS := http.FileServer(http.Dir("./frontend-zvukovka"))
+	http.Handle("/zvukovka/", http.StripPrefix("/zvukovka", zvukovkaFS))
+
 	http.ListenAndServe(":8000", nil)
 }
